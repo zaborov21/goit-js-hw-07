cb8b52596783332e896900e10584acb17de5080f
Andrii Zaborovskyi <zaborov21@gmail.com>
2023-06-04 21:33:50 +0200

    .
    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+
+const makeMarkup = () => {
+  const murkup = galleryItems.map(({ preview, original, description }) => {
+
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+
+  }).join("");
+  return murkup;
+}
+galleryRef.insertAdjacentHTML('beforeend', makeMarkup());
+
+function onGallery(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const largeImgUrl = e.target.dataset.source;
+  basicLightbox.create(`
+		<img width="1400" height="900" src="${largeImgUrl}">
+	`).show()
+}
+
+galleryRef.addEventListener("click", onGallery);
+
+document.addEventListener("keydown", e => {
+  e.preventDefault();
+  if (e.code === 'Escape') {
+    console.log("You are clicked: ", e.code);
+    
+  }
+  
+});
